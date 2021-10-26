993d97f923e08df0aecca0e2c2bac773959f2890
przemekkojs <przemek.kojs@gmail.com>
2021-10-26 20:31:04 +0200

    Lab 29 10
    Na laborki

---
+++ Lab_29_10/src/Prog/Prog.java
@@ -0,0 +1,12 @@
+package Prog;
+import Wielomian.Wielomian;
+
+public class Prog 
+{
+	public static void main(String[] args) 
+	{		
+		System.out.println(Wielomian.Czebyszew(0, 3));
+		System.out.println(Wielomian.Czebyszew(1, 3));
+		System.out.println(Wielomian.Czebyszew(4, 3));
+	}
+}
---
+++ Lab_29_10/src/Wielomian/Wielomian.java
@@ -0,0 +1,34 @@
+package Wielomian;
+
+public class Wielomian 
+{
+	public static double Czebyszew(int n, double x)
+	{
+		double T0 = 1;
+		double T1 = x;		
+		
+		if(n == 0)
+		{
+			return T0;
+		}
+		else if(n == 1)
+		{
+			return T1;
+		}
+		else if(n >= 2)
+		{
+			double T = (2 * x * T1) - T0;
+			
+			for(int i = 2; i < n; i++)
+			{
+				T0 = T1;
+				T1 = T;
+				T = (2 * x * T1) - T0;
+			}
+			
+			return T;
+		}
+		
+		return 0;
+	}
+}
