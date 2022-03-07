1dedc20511babb883f0a0960d90ac476a1e91e2c
przemekkojs <przemek.kojs@gmail.com>
2022-03-07 07:55:49 +0100

    AiSD

--- AiSD_2_03_22/src/Ver2/Main.java
+++ AiSD_2_03_22/src/Ver2/Main.java
@@ -1,5 +1,7 @@
 package Ver2;
 
+import java.util.Random;
+
 public class Main 
 {
 	private static int n;
@@ -42,6 +44,8 @@ public class Main
 		Write("Oryginalna");
 		
 		int sum = onesCount + (2 * twosCount) + (3 * threesCount) + (4 * foursCount);
+		int index = 0;
+		long startTime = System.nanoTime();		
 		
 		if(sum < 3 || sum == 5)
 		{
@@ -51,10 +55,16 @@ public class Main
 		while(onesCount + twosCount > 0 && solution != -1)
 		{			
 			DealWithOnes();
-			DealWithTwos();			
+			DealWithTwos();		
+			
+			index++;
 		}	
 		
+		long endTime = System.nanoTime();
+		
 		System.out.println("WYNIK: " + solution + "<==========");
+		System.out.println("ILOŒÆ POWTÓRZEÑ PÊTLI: " + index);
+		System.out.println("CZAS: " + ((endTime - startTime)/1000000f) + " ms");
 		System.out.println();
 	}
 	
@@ -67,7 +77,7 @@ public class Main
 			System.out.print(x);
 		}
 		
-		System.out.println("(" + solution + ", " + modifier + ")");
+		System.out.println("(" + solution + ", -" + modifier + ")");
 	}
 	
 	private static void DealWithOnes()
@@ -99,7 +109,7 @@ public class Main
 			
 			solution++;
 			
-			Write("1 -> 2");	
+			Write("1,2 -> 0,3");	
 		}			
 		
 		if(onesCount > 0 && threesCount > 0) //Sumujemy jedynki z trójkami
@@ -115,12 +125,9 @@ public class Main
 			}
 			
 			a[oneIndex] = 0;
-			a[threeIndex] = 4;
+			a[threeIndex] = 4;			
 			
-			if(foursCount == 0)
-			{
-				modifier++;
-			}
+			modifier++;
 			
 			onesCount--;
 			threesCount--;
@@ -128,65 +135,69 @@ public class Main
 			
 			solution++;
 			
-			Write("1 -> 3");
+			Write("1,3 -> 0,4");
 		}				
 		
-		if(onesCount > 1) //Sumujemy jedynki z jedynkami
+		if(twosCount == 0)
 		{
-			while(oneIndex < n && a[oneIndex] != 1)
+			if(onesCount > 1) //Sumujemy jedynki z jedynkami
 			{
-				oneIndex++;
-			}
+				while(oneIndex < n && a[oneIndex] != 1)
+				{
+					oneIndex++;
+				}
+				
+				while(oneIndex2 < n && (a[oneIndex2] != 1 || oneIndex == oneIndex2))
+				{
+					oneIndex2++;
+				}
+				
+				a[oneIndex] = 0;
+				a[oneIndex2] = 2;
+				
+				onesCount -= 2;
+				twosCount++;
+				
+				solution++;
+				
+				Write("1,1 -> 0,2");
+			}		
 			
-			while(oneIndex2 < n && (a[oneIndex2] != 1 || oneIndex == oneIndex2))
+			if(onesCount > 0 && foursCount > 1) //Tworzymy 2,3 z 4,1
 			{
-				oneIndex2++;
-			}
-			
-			a[oneIndex] = 0;
-			a[oneIndex2] = 2;
-			
-			onesCount -= 2;
-			twosCount++;
-			
-			solution++;
-			
-			Write("1 -> 1");
+				while(oneIndex < n && a[oneIndex] != 1)
+				{
+					oneIndex++;
+				}
+				
+				while(fourIndex < n && a[fourIndex] != 4)
+				{
+					fourIndex++;
+				}
+				
+				a[oneIndex] = 2;
+				a[fourIndex] = 3;
+				
+				onesCount--;
+				foursCount--;
+				threesCount++;
+				twosCount++;
+				
+				solution++;
+				
+				Write("1,4 -> 2,3");		
+			}	
 		}		
-		
-		if(onesCount > 0 && foursCount > 1) //Tworzymy 2,3 z 4,1
-		{
-			while(oneIndex < n && a[oneIndex] != 1)
-			{
-				oneIndex++;
-			}
-			
-			while(fourIndex < n && a[fourIndex] != 4)
-			{
-				fourIndex++;
-			}
-			
-			a[oneIndex] = 2;
-			a[fourIndex] = 3;
-			
-			onesCount--;
-			foursCount--;
-			threesCount++;
-			twosCount++;
-			
-			solution++;
-			
-			Write("4 -> 1");		
-		}	
 	}
 	
 	private static void DealWithTwos()
 	{		
 		int twoIndex = 0;
-		int twoIndex2 = 0;			
-		int fourIndex = 0;
+		int twoIndex2 = 0;		
+		int threeIndex = 0;
+		int fourIndex = 0;			
 		
-		if(twosCount > 0 && foursCount > 0) //Z 2, 4 tworzymy 3, 3
+		if(twosCount > 0 && foursCount >= twosCount && onesCount == 0) //Z 2, 4 tworzymy 3, 3
 		{
 			while(twoIndex < n && a[twoIndex] != 2)
 			{
@@ -207,9 +218,37 @@ public class Main
 			
 			solution++;
 			solution -= modifier;
+			modifier = 0;
 			
-			Write("4 -> 2");
+			Write("2,4 -> 3,3");
 		}		
+		
+		if(twosCount > 0 && threesCount > 0) //Z 2, 3 tworzymy 1, 4
+		{
+			while(twoIndex < n && a[twoIndex] != 2)
+			{
+				twoIndex++;
+			}
+			
+			while(threeIndex < n && a[threeIndex] != 3)
+			{
+				threeIndex++;
+			}
+			
+			a[twoIndex] = 1;
+			a[threeIndex] = 4;
+			
+			twosCount--;
+			threesCount--;
+			foursCount++;
+			onesCount++;
+			
+			solution++;
+			solution -= modifier;
+			modifier = 0;
+			
+			Write("2,3 -> 1,4");
+		}			
 		
 		if(twosCount > 1) //Dodajemy dwójki do siebie
 		{
@@ -226,35 +265,33 @@ public class Main
 			a[twoIndex] = 0;
 			a[twoIndex2] = 4;
 			
-			if(foursCount == 0)
-			{
-				modifier++;
-			}
+			modifier++;
 			
 			twosCount -= 2;
 			foursCount++;
 						
-			solution += 2;
+			solution++;
 			
 			Write("2,2 -> 4");
 		}		
 	}
 	
-	public static void main(String[] args) 
+	private static byte[] RandomTab()
 	{
-		Solve(new byte[] {1, 1, 1, 1, 1, 1});
-		Solve(new byte[] {1, 1, 1, 1, 1, 1, 1});
-		Solve(new byte[] {1, 1, 1, 1, 1, 1, 1, 1});
-		Solve(new byte[] {1, 2, 2, 4, 3});
-		Solve(new byte[] {4, 1, 1});
-		Solve(new byte[] {0, 3, 0, 4});
-		Solve(new byte[] {1});
-		Solve(new byte[] {1, 2});
-		Solve(new byte[] {2, 2, 1, 1});		
-		Solve(new byte[] {2, 2, 3});
-		Solve(new byte[] {4, 1});
-		Solve(new byte[] {4, 3, 2});
-		Solve(new byte[] {2, 3, 2, 3, 2});
-		Solve(new byte[] {2, 2, 2});
+		Random random = new Random();
+		
+		byte[] tab = new byte[random.nextInt(1, 21)];	
+
+		for(int i = 0; i < tab.length; i++)
+		{
+			tab[i] = (byte)random.nextInt(0, 5);
+		}
+		
+		return tab;
+	}
+	
+	public static void main(String[] args) 
+	{		
+		Solve(RandomTab());
 	}
 }
